fd0a1933e134720b20d2ebe5895dffa021dff536
BRAET Sergio (s) <sergio.braet@dt-c205-10.local>
2018-05-22 15:41:54 +0200

    Initial Commit

---
+++ LaFonderie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaFonderie
+//
+//  Created by BRAET Sergio (s) on 22/05/2018.
+//  Copyright © 2018 BRAET Sergio (s). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
